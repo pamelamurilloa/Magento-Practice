0c211cd8b49a7aee8431b0d8327c43b187d293ca
Pamela Murillo <pmurillo@kalicr.com>
2026-01-27 16:04:43 -0600

    Translations done and the start of a new module made, a table has been created

--- magento/app/code/Kalicr/CustomCheckoutProcess/Controller/Adminhtml/Logs/Index.php
+++ magento/app/code/Kalicr/CustomCheckoutProcess/Controller/Adminhtml/Logs/Index.php
@@ -9,8 +9,8 @@ class Index extends Action
     protected $resultPageFactory;
 
     public function __construct(
-        Action\Context $context,
-        PageFactory $resultPageFactory
+        Action\Context $context, // this appears to always be present, it constructs the page as the url is called
+        PageFactory $resultPageFactory // this is needed to create the result page
     ) {
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
@@ -18,13 +18,13 @@ class Index extends Action
 
     public function execute()
     {
-        $resultPage = $this->resultPageFactory->create();
-        $resultPage->getConfig()->getTitle()->prepend(__('Order JSON Logs'));
+        $resultPage = $this->resultPageFactory->create(); // this loads the layout and other necessary elements for the admin page
+        $resultPage->getConfig()->getTitle()->prepend(__('Order JSON Logs')); // set the page title
         return $resultPage;
     }
 
     protected function _isAllowed()
     {
-        return $this->_authorization->isAllowed('Kalicr_CustomCheckoutProcess::order_log');
+        return $this->_authorization->isAllowed('Kalicr_CustomCheckoutProcess::order_log'); // permission check to access this page
     }
 }
